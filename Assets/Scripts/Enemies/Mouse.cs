7446a2d748e48a5fa4da9e67000488c848f21f4d
chrisJohns0n <126682002+ChrisJohnson0110@users.noreply.github.com>
2025-04-05 17:45:30 +0100

    MouseFixed

--- Assets/Scripts/Enemies/Mouse.cs
+++ Assets/Scripts/Enemies/Mouse.cs
@@ -8,60 +8,98 @@ public class Mouse : MonoBehaviour
     private GameObject targetObject;
     [SerializeField] private GameObject mouseHole;
     [SerializeField] private float speed;
-    private bool isTouchingTarget = false;
     [HideInInspector] public FixedJoint joint;
+
+    public Vector2 roomMinBounds = new Vector2(-5f, -5f);
+    public Vector2 roomMaxBounds = new Vector2(5f, 5f);
+    private Vector3 targetPosition;
+
 
     private void Update()
     {
-        if (isTouchingTarget == true) //move to hole
+        if (targetObject == null) //if no target get one
         {
-            transform.position = Vector3.MoveTowards(gameObject.transform.position, mouseHole.transform.position, speed);
+            GetNearest();
+            if (targetObject == null)
+            {
+                MoveToRandom();
+            }
+        }
+        else
+        {
+            if (joint != null) //if connected to object, take it to hole
+            {
+                MoveToHole();
+            }
+            else //if not connected move to object
+            {
+                MoveToTarget();
+            }
+
+            if (targetObject.gameObject.GetComponent<PickupObject>().isHeld == true)
+            {
+                Disconnect();
+            }
+        }
+    }
+
+    private void GetNearest()
+    {
+        pickupObjects = new List<GameObject>();
+
+        foreach (PickupObject g in GameObject.FindObjectsOfType<PickupObject>())
+        {
+            if (g.GetComponent<PickupObject>().isHeld == false)
+            {
+                pickupObjects.Add(g.gameObject);
+            }
+        }
+        foreach (GameObject go in pickupObjects) //get nearest
+        {
+            if (targetObject == null)
+            {
+                targetObject = go;
+            }
+            else if (Vector3.Distance(gameObject.transform.position, go.transform.position) < Vector3.Distance(gameObject.transform.position, targetObject.transform.position))
+            {
+                targetObject = go;
+            }
+        }
+    }
+
+    private void MoveToHole()
+    {
+        transform.position = Vector3.MoveTowards(gameObject.transform.position, mouseHole.transform.position, speed);
+        transform.position = new Vector3(transform.position.x, 0.47f, transform.position.z);
+    }
+
+    private void MoveToTarget()
+    {
+        transform.position = Vector3.MoveTowards(gameObject.transform.position, targetObject.transform.position, speed);
+        transform.position = new Vector3(transform.position.x, 0.47f, transform.position.z);
+    }
+    private void MoveToRandom()
+    {
+        if (targetPosition == Vector3.zero)
+        {
+            targetPosition = new Vector3(
+                Random.Range(roomMinBounds.x, roomMaxBounds.x),
+                transform.position.y, // Keep same Y height
+                Random.Range(roomMinBounds.y, roomMaxBounds.y)
+            );
+        }
+        if (Vector3.Distance(transform.position, targetPosition) > 0.1f)
+        {
+            transform.position = Vector3.MoveTowards(gameObject.transform.position, targetPosition, speed);
             transform.position = new Vector3(transform.position.x, 0.47f, transform.position.z);
         }
         else
         {
-            if (targetObject == null)
-            {
-                foreach (PickupObject g in GameObject.FindObjectsOfType<PickupObject>())
-                {
-                    if (g.GetComponent<PickupObject>().isHeld == false)
-                    {
-                        pickupObjects.Add(g.gameObject);
-                    }
-                }
-                foreach (GameObject go in pickupObjects) //get nearest
-                {
-                    if (targetObject == null)
-                    {
-                        targetObject = go;
-                    }
-                    else if (Vector3.Distance(gameObject.transform.position, go.transform.position) < Vector3.Distance(gameObject.transform.position, targetObject.transform.position))
-                    {
-                        targetObject = go;
-                    }
-                }
-            }
-            if (targetObject != null)
-            {
-                transform.position = Vector3.MoveTowards(gameObject.transform.position, targetObject.transform.position, speed);
-                transform.position = new Vector3(transform.position.x, 0.47f, transform.position.z);
-            }
-        }
-        if (targetObject != null)
-        {
-            if (targetObject.GetComponent<PickupObject>().isHeld == true)
-            {
-                pickupObjects.Remove(targetObject);
-                targetObject = null;
-                isTouchingTarget = false;
-                if (joint != null)
-                {
-                    Destroy(joint);
-                }
-
-            }
+            targetPosition = Vector3.zero;
         }
     }
+
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.GetComponent<PickupObject>())
@@ -70,59 +108,46 @@ public class Mouse : MonoBehaviour
             {
                 if (other.gameObject == targetObject)
                 {
-                    StartCoroutine(Delay(other.gameObject));
+                    Connect();
                 }
             }
         }
     }
 
-    private void OnTriggerStay(Collider other)
-    {
-        if (targetObject == other.gameObject)
-        {
-            isTouchingTarget = true;
-        }
-    }
-
-    IEnumerator Delay(GameObject target)
+    IEnumerator Delay()
     {
         yield return new WaitForSeconds(2);
-
-        if (targetObject != null)
-        {
-            if (targetObject.GetComponent<PickupObject>().isHeld == false)
-            {
-                if (isTouchingTarget == true)
-                {
-                    Rigidbody rb = target.GetComponent<Rigidbody>();
-                    if (rb != null && !rb.isKinematic)
-                    {
-                        joint = gameObject.AddComponent<FixedJoint>();
-                        joint.connectedBody = target.gameObject.GetComponent<Rigidbody>();
-                    }
-                }
-                
-            }
-        }
-
-        
     }
 
 
 
     private void Connect()
     {
-        //make a joint connection to the object
-        Rigidbody rb = target.GetComponent<Rigidbody>();
-        if (rb != null && !rb.isKinematic)
+        //StartCoroutine(Delay()); //add before move
+
+        if (targetObject != null)
         {
-            joint = gameObject.AddComponent<FixedJoint>();
-            joint.connectedBody = target.gameObject.GetComponent<Rigidbody>();
+            if (targetObject.GetComponent<PickupObject>().isHeld == false)
+            {
+                //make a joint connection to the object
+                Rigidbody rb = targetObject.GetComponent<Rigidbody>();
+                if (rb != null && !rb.isKinematic)
+                {
+                    joint = gameObject.AddComponent<FixedJoint>();
+                    joint.connectedBody = targetObject.gameObject.GetComponent<Rigidbody>();
+                }
+            }
         }
     }
 
     private void Disconnect()
     {
+        pickupObjects.Remove(targetObject);
+        targetObject = null;
 
+        if (joint != null)
+        {
+            Destroy(joint);
+        }
     }
 }
